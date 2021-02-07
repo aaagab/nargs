20ce81974fdee5f9a2d9b4f7221d584145c50255
gabaaa <not-set>
2021-02-07 12:00:38 -0600

    first commit after clone

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# authors: Gabriel Auger
+# name: Nested Arguments
+# licenses: MIT 
+__version__ = "0.0.0"
+
+# from .dev.nargs import nargs
+# from .gpkgs import message as msg
+# from .gpkgs.options import Options
+# from .gpkgs.etconf import Etconf
